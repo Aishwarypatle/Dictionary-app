b613891d481529af319debe57d8e11b4060f0efd
Aishwary P <aishwaryp@Aishwarys-MacBook-Air.local>
2024-02-13 18:33:46 +0530

    project initiated

---
+++ app/src/components/Meaning.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Meaning = () => {
+  return (
+    <div>
+        <h1>this is meaning compo</h1>
+    </div>
+  )
+}
+
+export default Meaning
---
+++ app/src/components/SearchBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchBox = () => {
+  return (
+    <div>
+        <h1>This is search component</h1>
+    </div>
+  )
+}
+
+export default SearchBox
